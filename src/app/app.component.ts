3cf79ebdb07519a230ad348a5b28a0d3e4fcd8bd
Tung Ly <tyanhly@gmail.com>
2017-01-04 21:48:09 +0700

    init home page

---
+++ src/app/app.component.ts
@@ -0,0 +1,32 @@
+import { Component , ViewChild } from '@angular/core';
+import { Config, Menu, NavController, Platform} from 'ionic-angular';
+import { StatusBar, Splashscreen } from 'ionic-native';
+
+import { HomePage } from '../pages/home/home';
+
+@Component({
+  templateUrl: 'app.html'
+})
+export class MyApp {
+  rootPage = HomePage;
+  @ViewChild('content') content: NavController;
+  @ViewChild(Menu) menu: Menu;
+  constructor(platform: Platform) {
+    platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      StatusBar.styleDefault();
+      Splashscreen.hide();
+    });
+  }
+
+  pages = [
+    { title: 'Home', component: HomePage },
+
+  ];
+  openPage(page) {
+    // Reset the content nav to have just this page
+    // we wouldn't want the back button to show in this scenario
+    this.content.setRoot(page.component);
+  }
+}
